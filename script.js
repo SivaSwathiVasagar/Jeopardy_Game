6b0235b4bacb70ea5531d2183e0e7e85aa12cbbe
Siva Swathi Vasagar <sivaswathi.vasagar@gmail.com>
2023-08-19 13:28:19 -0400

    updating score function

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let cardsArray = document.querySelectorAll(".card");
 let forms = document.querySelectorAll(".form");
+let onScreenStatus;
+let gameStatus = document.getElementById("status");
+gameStatus.style.fontSize = '24px';
+let score = 0;
+let addScore = document.getElementById("score");
+addScore.textContent= score;
 
 cardsArray.forEach((card) => {
     card.addEventListener("click",()=>{
@@ -8,17 +14,65 @@ cardsArray.forEach((card) => {
 });
 
 function result(form){
+    onScreenStatus = " Started ";
+    gameStatus.textContent = onScreenStatus;
     let formData = new FormData(form);
-    console.log(formData);
     let userSelectedAnswer = formData.get("question");
-    if(userSelectedAnswer === "correct"){
-       alert(" Correct Answer");
+    let makeUserSelectedQuestionVisible = document.querySelector(".question-overlay");
+    makeUserSelectedQuestionVisible.classList.add("visible");
+    if((userSelectedAnswer === "correct-100") || (userSelectedAnswer === "correct-200") || (userSelectedAnswer === "correct-300"))
+    {
+        alert("Correct Answer, You got it.");
+
+        if (userSelectedAnswer === "correct-100")
+        {
+            score += 100;
+        }
+        if (userSelectedAnswer === "correct-200")
+        {
+            score += 200;
+        }
+        if (userSelectedAnswer === "correct-300")
+        {
+            score += 300;
+        }
     }
-    else{
-        alert(" Incorrect Answer");
+    else if((userSelectedAnswer !== "correct-100") || (userSelectedAnswer !== "correct-200") || (userSelectedAnswer !== "correct-300"))
+    {
+        alert("Incorrect Answer, You missed it.");
+
+        if (userSelectedAnswer === "incorrect-100")
+        {
+            score -= 100;
+        }
+        if (userSelectedAnswer === "incorrect-200")
+        {
+            score -= 200;
+        }
+        if (userSelectedAnswer === "incorrect-300")
+        {
+            score -= 300;
+        }  
     }
-    
+    else if (userSelectedAnswer === null)
+    {     
+     alert("Please select an option before submitting.");
+    }
+
+    addScore.textContent= score;
+    if(score === 1800){
+        onScreenStatus = " Completed ";
+        gameStatus.textContent = onScreenStatus;
+        alert ("You WIN !!!");
+    }
 }
+
+
+
+
+         
+     
+
 
 
 
